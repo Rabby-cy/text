c3ce800953df86710197283e4dc5084f5c297d6c
Rabby-cy <916622504@qq.com>
2026-01-09 00:01:59 +0800

    2

---
+++ 金鸢尾兰鼠族/基因/彩蛋/部分废案/BloodTransfusion_Patch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace OberoniaAureaGene;
+
+//特异血原相关
+[StaticConstructorOnStartup]
+[HarmonyPatch(typeof(Recipe_BloodTransfusion), "AvailableOnNow")]
+public static class BloodTransfusion_Patch
+{
+    [HarmonyPostfix]
+    public static void Postfix(ref bool __result, Thing thing)
+    {
+        if (__result && thing is Pawn pawn && pawn.genes != null)
+        {
+            if (pawn.genes.HasGene(OberoniaAureaGeneDefOf.OAGene_SpecificHemogen))
+            {
+                __result = false;
+            }
+        }
+    }
+}
---
+++ 金鸢尾兰鼠族/基因/彩蛋/部分废案/BloodfeederBite_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace OberoniaAureaGene;
+
+//特异血原相关
+[StaticConstructorOnStartup]
+[HarmonyPatch(typeof(CompAbilityEffect_BloodfeederBite), "Valid")]
+public static class BloodfeederBite_Patch
+{
+    [HarmonyPostfix]
+    public static void Postfix(ref bool __result, LocalTargetInfo target)
+    {
+        if (__result)
+        {
+            Pawn pawn = target.Pawn;
+            if (pawn.genes != null && pawn.genes.HasGene(OberoniaAureaGeneDefOf.OAGene_SpecificHemogen))
+            {
+                __result = false;
+            }
+        }
+    }
+}
